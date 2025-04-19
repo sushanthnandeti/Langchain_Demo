99377d0edee3115c57154f5ef9a0f6fb0eb0979e
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-18 22:29:31 -0400

    Pydantic Parser added to format the response data

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ import os
 from  dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import HumanMessagePromptTemplate, ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain.output_parsers import PydanticOutputParser
+
+
+class Country(BaseModel):
+    capital: str = Field(description = "Capital of the Country")
+    name: str = Field(description="Name of the Country")
 
 load_dotenv()
 
 OPENAI_MODEL = 'gpt-4'
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-PROMPT_COUNTRY_INFO =""" Provide information about {country}."""
+PROMPT_COUNTRY_INFO =""" Provide information about {country}. {format_instructions}"""
 
 def main():
     llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model_name = OPENAI_MODEL)
+    parser = PydanticOutputParser(pydantic_object=Country)
+
 
     # get user input 
     country = input(" Enter the name of the country ")
 
     message = HumanMessagePromptTemplate.from_template(template=PROMPT_COUNTRY_INFO)
     chat_prompt = ChatPromptTemplate.from_messages(messages = [message])
-    chat_prompt_with_values = chat_prompt.format_prompt(country = country)
+    chat_prompt_with_values = chat_prompt.format_prompt(country = country, format_instructions = parser.get_format_instructions())
 
     response = llm(chat_prompt_with_values.to_messages())
-    print(response)
+    data = parser.parse(response.content)
+    print(f'The capital of {data.name} is {data.capital}')
 
 if __name__ == "__main__": 
     main()  

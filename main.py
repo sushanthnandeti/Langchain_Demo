0975db71ffcf85628439f4aa5821bd4f21e7f7fc
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-18 22:12:24 -0400

    Added Prompt template and user input for tuned response

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os 
 from  dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import HumanMessagePromptTemplate, ChatPromptTemplate
+
 load_dotenv()
 
 OPENAI_MODEL = 'gpt-4'
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
+PROMPT_COUNTRY_INFO =""" Provide information about {country}."""
+
 def main():
     llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model_name = OPENAI_MODEL)
-    result = llm.predict(" Who are the highest selling rappers of all time. Give the ranking from highest to lowest")
-    print(result)
+
+    # get user input 
+    country = input(" Enter the name of the country ")
+
+    message = HumanMessagePromptTemplate.from_template(template=PROMPT_COUNTRY_INFO)
+    chat_prompt = ChatPromptTemplate.from_messages(messages = [message])
+    chat_prompt_with_values = chat_prompt.format_prompt(country = country)
+
+    response = llm(chat_prompt_with_values.to_messages())
+    print(response)
 
 if __name__ == "__main__": 
-    main()
+    main()  
